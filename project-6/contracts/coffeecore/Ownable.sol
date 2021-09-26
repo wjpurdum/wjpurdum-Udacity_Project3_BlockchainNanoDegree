4c662d6fb84af8feb5d405a14982db0df3f4e5d0
wjpurdum <whitney.purdum@gmail.com>
2021-09-26 16:50:14 -0400

    add require message

--- project-6/contracts/coffeecore/Ownable.sol
+++ project-6/contracts/coffeecore/Ownable.sol
@@ -20,7 +20,7 @@ contract Ownable {
 
     /// Define a function modifier 'onlyOwner'
     modifier onlyOwner() {
-        require(isOwner());
+        require(isOwner(), "Must be contract owner to call method");
         _;
     }
 
@@ -42,7 +42,7 @@ contract Ownable {
 
     /// Define an internal function to transfer ownership
     function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0));
+        require(newOwner != address(0), "Invaid Address");
         emit TransferOwnership(origOwner, newOwner);
         origOwner = newOwner;
     }
